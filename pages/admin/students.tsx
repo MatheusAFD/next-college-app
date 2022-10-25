fb0913b6c345e6ca13efc96b81cc63d0047a789b
matheusafd <matheus.daf@outlook.com>
2022-10-25 11:22:39 -0300

    render student list

--- pages/admin/students.tsx
+++ pages/admin/students.tsx
@@ -1,3 +1,12 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  useDeleteOneStudentMutation,
+  useGetStudentsQuery,
+} from "../../src/graphql/generated";
+
+const Home: NextPage = () => {
   const { data, refetch } = useGetStudentsQuery();
 
   const [deleteOneStudent] = useDeleteOneStudentMutation();
@@ -13,3 +22,32 @@
 
     refetch();
   }
+
+  const router = useRouter();
+  return (
+    <>
+      <h1>Students</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h2 onClick={() => router.push("/")}>Home</h2>
+
+      <ul>
+        {data?.students.nodes.map((student) => {
+          return (
+            <li
+              onClick={() => {
+                deleteOne(student.id);
+              }}
+              key={student.id}
+            >
+              {student.name}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
