dfea51378a4ec2ee6e264f6b1b2e937e5d589ca9
matheusafd <matheus.daf@outlook.com>
2022-10-26 18:19:48 -0300

    :sparkles: functions for increment and decrement pagination

---
+++ src/features/StudentsTable/studentsTable.tsx
@@ -0,0 +1,11 @@
+  function setNextPage() {
+    if (hasNextPage) {
+      setPage(page + 1);
+    }
+  }
+
+  function setPreviusPage() {
+    if (hasPreviusPage) {
+      setPage(page - 1);
+    }
+  }
